6cb5f00bd8b8151e0e30b6dc881751c14d3cc06b
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-09-14 02:46:11 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,26 @@
   <div id="app" class="d-flex flex-column min-vh-100">
     <div class="row mx-auto py-3">
       <b-card class="m-3 p-5" title="Vixen Media" sub-title="Model Search">
-        <b-form-input v-model="pcModel" class="mt-5" type="search"></b-form-input>
-        <b-button @click="openPremiumContentPortals" class="mt-3 font-weight-bold" block variant="danger" type="submit">Open</b-button>
-        <b-button @click="openOtherSites" class="mt-3 font-weight-bold" block variant="outline-danger">Other Sites</b-button>
+        <b-form-input
+          v-model="pcModel"
+          class="mt-5"
+          type="search"
+        ></b-form-input>
+        <b-button
+          @click="openPremiumContentPortals"
+          class="mt-3 font-weight-bold"
+          block
+          variant="danger"
+          type="submit"
+          >Open</b-button
+        >
+        <b-button
+          @click="openOtherSites"
+          class="mt-3 font-weight-bold"
+          block
+          variant="outline-danger"
+          >Other Sites</b-button
+        >
       </b-card>
     </div>
   </div>
@@ -70,12 +87,22 @@ export default {
 </script>
 
 <style>
+/* Dark Theme Styles */
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000; /* Set text color for dark theme */
+  background-color: #121212; /* Set background color for dark theme */
   margin-top: 60px;
 }
+
+/* Additional dark theme styles for specific elements if needed */
+.b-card {
+  background-color: #1e1e1e;
+  color: #ffffff;
+}
+
+/* Add more styles as needed for specific elements */
 </style>
