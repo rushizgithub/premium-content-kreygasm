0fa6872344942a587da437f8ff34489c475aa5c3
Rushi Ranpise <136841749+rushizgithub@users.noreply.github.com>
2023-09-14 02:37:45 +0530

    1

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,29 @@
 <template>
   <div id="app" class="d-flex flex-column min-vh-100">
     <div class="row mx-auto py-3">
-      <!-- <b-card
-        v-if="!loggedInModel"
-        class="m-3 p-5"
-        title="Login"
-        sub-title="password"
-      >
-        <b-form ref="authForm" @submit.prevent="logIn()">
-          <b-input-group class="mt-5">
-            <template #prepend>
-              <b-input-group-text
-                ><b-icon icon="key"></b-icon
-              ></b-input-group-text>
-            </template>
-            <b-form-input v-model="pwModel" type="password" required />
-          </b-input-group>
-          <b-button
-            class="mt-3 font-weight-bold"
-            block
-            variant="success"
-            type="submit"
-          >
-            Log In
-          </b-button>
-        </b-form>
-      </b-card> -->
       <b-card class="m-3 p-5" title="Vixen Media" sub-title="Model Search">
-        <b-form-input
-          class="mt-5"
-          v-model="pcModel"
-          type="search"
-        ></b-form-input>
-        <b-button
-          class="mt-3 font-weight-bold"
-          block
-          variant="danger"
-          type="submit"
-          @click="openPremiumContentPortals"
-        >
-          Open
-        </b-button>
-        <b-button
-          class="mt-3 font-weight-bold"
-          block
-          variant="outline-danger"
-          @click="openPremiumContentBank"
-        >
-          Other Sites
-        </b-button>
+        <b-form-input v-model="pcModel" class="mt-5" type="search"></b-form-input>
+        <b-button @click="openPremiumContentPortals" class="mt-3 font-weight-bold" block variant="danger" type="submit">Open</b-button>
+        <b-button @click="openPremiumContentBank" class="mt-3 font-weight-bold" block variant="outline-danger">Other Sites</b-button>
       </b-card>
     </div>
   </div>
 </template>
 
 <script>
+const PASSWORD = "password";
+
 export default {
   name: "App",
   data() {
     return {
-      loggedInModel: localStorage.pw === "password" ? true : false,
       pwModel: "",
       pcModel: "",
-      dfModel: { val: localStorage.lastPostBatch || "", maxLink: 0 },
     };
   },
-  mounted() {},
   methods: {
     logIn() {
-      if (this.pwModel === "password") {
+      if (this.pwModel === PASSWORD) {
         localStorage.pw = this.pwModel;
         alert("Welcome!");
         this.$refs.authForm.submit();
@@ -76,51 +31,39 @@ export default {
         alert("Wrong!");
       }
     },
+    openUrl(url) {
+      window.open(url, Math.random());
+    },
     openPremiumContentPortals() {
-      var pcModelKebab = this.pcModel.replace(" ", "-");
-      if (pcModelKebab === "") {
-        pcModelKebab = "videos";
-      } else {
-        pcModelKebab = "models/" + pcModelKebab;
-      }
-      window.open("https://www.vixen.com/" + pcModelKebab, Math.random());
-      window.open("https://www.tushy.com/" + pcModelKebab, Math.random());
-      window.open("https://www.tushyraw.com/" + pcModelKebab, Math.random());
-      window.open("https://www.blacked.com/" + pcModelKebab, Math.random());
-      window.open("https://www.blackedraw.com/" + pcModelKebab, Math.random());
-      window.open("https://www.deeper.com/" + pcModelKebab, Math.random());
-      window.open("https://www.milfy.com/" + pcModelKebab, Math.random());
-      window.open("https://www.slayed.com/" + pcModelKebab, Math.random());
+      const pcModelKebab = this.pcModel.trim().replace(/\s+/g, "-") || "videos";
+      const portals = [
+        "vixen.com",
+        "tushy.com",
+        "tushyraw.com",
+        "blacked.com",
+        "blackedraw.com",
+        "deeper.com",
+        "milfy.com",
+        "slayed.com",
+      ];
+
+      portals.forEach((portal) => {
+        this.openUrl(`https://www.${portal}/${pcModelKebab}`);
+      });
     },
     openPremiumContentBank() {
-      var pcModelKebab1 = this.pcModel.replace(" ", "-");
-      if (pcModelKebab1 === "") {
-        pcModelKebab1 = "";
-      } else {
-        pcModelKebab1 = "stars/" + pcModelKebab1;
-      }
-      window.open(
-        "https://tube.perverzija.com/" + pcModelKebab1,
-        Math.random()
-      );
-      var pcModelKebab2 = this.pcModel.replace(" ", "-");
-      if (pcModelKebab2 === "") {
-        pcModelKebab2 = "";
-      } else {
-        pcModelKebab2 = "tag/" + pcModelKebab1;
-      }
-      window.open("https://porndish.com/" + pcModelKebab2, Math.random());
-      var pcModelKebab3 = this.pcModel.replace(" ", "-");
-      if (pcModelKebab3 === "") {
-        pcModelKebab3 = "";
-      } else {
-        pcModelKebab3 = "tag/" + pcModelKebab3 + ".html";
-      }
-      window.open("https://sxyprn.com/" + pcModelKebab3, Math.random());
+      const pcModelKebab = this.pcModel.trim().replace(/\s+/g, "-") || "";
+      const banks = [
+        "tube.perverzija.com",
+        "porndish.com",
+        "sxyprn.com",
+      ];
+
+      banks.forEach((bank) => {
+        this.openUrl(`https://${bank}/${pcModelKebab}`);
+      });
+
       this.pcModel = "";
-    },
-    postIdMax() {
-      return parseInt(this.dfModel.val) + parseInt(this.dfModel.maxLink);
     },
   },
 };
@@ -130,11 +73,6 @@ export default {
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
